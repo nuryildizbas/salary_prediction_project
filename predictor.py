565f5c11688a6ce1852091797ea014bb344753f6
nuryildizbas <yildizbas.nur@gmail.com>
2026-09-08 14:15:18 +0300

    formed x and y

--- predictor.py
+++ predictor.py
@@ -5,3 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
+df=pd.read_csv("Salary_dataset.csv")
+
+print(df.head())
